a344c7ecb496284f68334b2a4dc75de9bf76dde4
Denisov-Egor <programming2028@gmail.com>
2026-08-29 20:14:27 +0300

    исправлениеошибки при перключение языка

--- hyprland.lua
+++ hyprland.lua
@@ -329,7 +329,7 @@ hl.config({
         kb_layout = "us,ru",
         kb_variant = "",
         kb_model = "",
-        kb_options = "grp:alt_shift_toggle", "grp:win_space_toggle",
+        kb_options = "grp:win_space_toggle,grp:alt_shift_toggle",
         kb_rules = "",
 
         follow_mouse = 1,
